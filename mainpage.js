486ef7acd3e65687c1cf88e31fe167b6a8e04b56
Tim14159 <Oberdammer.Tim@gmx.at>
2025-04-21 17:06:54 +0200

    Add files via upload

---
+++ mainpage.js
@@ -0,0 +1,11 @@
+document.querySelector('.button-grid button:first-child').addEventListener('click', function() 
+{window.location.href = 'h_delivery.html'})
+
+document.querySelector('.button-grid button:nth-child(2)').addEventListener('click', function() 
+{window.location.href = 'h_banking.html'})
+
+document.querySelector('.button-grid button:nth-child(3)').addEventListener('click', function() 
+{window.location.href = 'h_postal.html'})
+
+document.querySelector('.button-grid button:nth-child(4)').addEventListener('click', function() 
+{window.location.href = 'h_charging.html'})
